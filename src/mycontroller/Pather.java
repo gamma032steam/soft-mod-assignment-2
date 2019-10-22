729ca6fdf6dea2fcf243f4b2eef2404678f7627f
Steven Nguyen <stevenhuyn@gmail.com>
2019-10-22 15:03:40 +1100

    dictionary constants instead of switches :)

--- src/mycontroller/Pather.java
+++ src/mycontroller/Pather.java
@@ -9,6 +9,36 @@ import java.util.*;
 
 public class Pather {
 
+    private static Integer SMALL_WEIGHT = -1000;
+    private static Integer AVOID_WEIGHT = 1000;
+    private static Integer IMPASSABLE_WEIGHT = null;
+    private static Integer DEFAULT_WEIGHT = 1;
+
+    private static HashMap<String, Integer> DEFAULT_TRAP_WEIGHT = defaultTrapWeightInitialiser();
+
+    private static HashMap<String, Integer> defaultTrapWeightInitialiser() {
+        HashMap<String, Integer> defaultTrapWeight = new HashMap<String, Integer>;
+        defaultTrapWeight.put("lava", AVOID_WEIGHT);
+        defaultTrapWeight.put("parcel", DEFAULT_WEIGHT);
+        defaultTrapWeight.put("mud", IMPASSABLE_WEIGHT);
+        defaultTrapWeight.put("water", DEFAULT_WEIGHT);
+        defaultTrapWeight.put("health", DEFAULT_WEIGHT);
+        defaultTrapWeight.put("grass", AVOID_WEIGHT);
+        return defaultTrapWeight
+    }
+
+    private static HashMap<MapTile.Type, Integer> DEFAULT_NON_TRAP_WEIGHT = defaultNonTrapWeightInitialiser();
+
+    private static HashMap<MapTile.Type, Integer> defaultNonTrapWeightInitialiser() {
+        HashMap<String, Integer> defaultNonTrapWeight = new HashMap<String, Integer>;
+        defaultNonTrapWeight.put(MapTile.Type.WALL, IMPASSABLE_WEIGHT);
+        defaultNonTrapWeight.put(MapTile.Type.UTILITY, DEFAULT_WEIGHT);
+        defaultNonTrapWeight.put(MapTile.Type.START, DEFAULT_WEIGHT);
+        defaultNonTrapWeight.put(MapTile.Type.FINISH, DEFAULT_WEIGHT);
+        defaultNonTrapWeight.put(MapTile.Type.ROAD, DEFAULT_WEIGHT);
+        defaultNonTrapWeight.put(MapTile.Type.EMPTY, DEFAULT_WEIGHT);
+    }
+
     /**
      * Gets this cool shit and finds da way
      * @param root
@@ -44,6 +74,38 @@ public class Pather {
         }
     }
 
+    private static boolean validateCoordinate(Coordinate candidate) {
+        if (candidate.x >= 0 && candidate.x < World.MAP_WIDTH) {
+            if (candidate.y >= 0 && candidate.y < World.MAP_HEIGHT) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static Integer weighTile(MapTile tile, MapTile objective) {
+        if (tile.getType().equals(MapTile.Type.TRAP)) {
+            return weighTrap((TrapTile) tile, objective);
+        } else if (tile.getType().equals(objective.getType())) {
+            return SMALL_WEIGHT;
+        } else {
+            return DEFAULT_NON_TRAP_WEIGHT.get(tile.getType());
+        }
+
+    }
+
+    private static Integer weighTrap(TrapTile trap, MapTile objective) {
+        // If objective is also a trap
+        if (objective.getType().equals(MapTile.Type.TRAP)) {
+            TrapTile objectiveTrap = (TrapTile) objective;
+            if (trap.getTrap().equals(objectiveTrap.getTrap())) {
+                return SMALL_WEIGHT;
+            }
+        }
+        return DEFAULT_TRAP_WEIGHT.get(trap.getTrap());
+    }
+
+    /*
     private static ArrayList<Coordinate> getNeighbours(HashMap<Coordinate, MapTile> explored, Coordinate root, ) {
         ArrayList<Coordinate> neighbours = new ArrayList<>();
         Coordinate candidate;
@@ -61,34 +123,40 @@ public class Pather {
         if (weighCoordinate(candidate)) neighbours.add(candidate);
     }
 
-    /**
-     * determines if the coordinate is within map boundaries and a "road" tile
-     * Meanings:
-     * -1: Impassable, i.e. wall
-     * INT_MAX: Last resort, but techincally passable, like lava
-     * @param candidate
-     * @return
-     */
+
+
     private static Integer weighCoordinate(HashMap<Coordinate, MapTile> explored, Coordinate candidate) {
         if (candidate.x >= 0 && candidate.x < World.MAP_WIDTH) {
             if (candidate.y >= 0 && candidate.y < World.MAP_HEIGHT) {
                 MapTile tile = explored.get(candidate);
-                if (tile.getType().equals(MapTile.Type.TRAP)) {
-                    TrapTile trap = (TrapTile) tile;
-                    switch (trap.getTrap()) {
-                        case "lava":
-                            return 10000;
-                        case "mud":
-                            return -1;
-                        case "water":
-                            return 1;
-                        case "parcel":
-                            return Integer.MIN_VALUE;
 
-
-                    }
-                }
             }
         }
     }
+
+    private static int weighTile(MapTile tile) {
+        if (tile.getType().equals(MapTile.Type.TRAP)) {
+            TrapTile trap = (TrapTile) tile;
+            switch (trap.getTrap()) {
+                case "lava":
+                    return 10000;
+                case "mud":
+                    return -1;
+                case "water":
+                    return 1;
+                case "parcel":
+                    return Integer.MIN_VALUE;
+            }
+        } else {
+            switch (tile.getType()) {
+                case WALL:
+                    return null;
+                case ROAD:
+                    return 1
+                case EXIT:
+
+            }
+        }
+    }
+    */
 }
