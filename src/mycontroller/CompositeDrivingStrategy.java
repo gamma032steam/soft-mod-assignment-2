9819c6e60a6adc2c69870d7f3de714246f837797
Gatlee Kaw <gatlee.kaw@gmail.com>
2019-10-23 14:27:31 +1100

    Add composite driving strategy

---
+++ src/mycontroller/CompositeDrivingStrategy.java
@@ -0,0 +1,42 @@
+package mycontroller;
+
+import controller.CarController;
+import tiles.MapTile;
+import utilities.Coordinate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CompositeDrivingStrategy extends DrivingStrategy {
+
+    private ArrayList<DrivingStrategy> strategies = new ArrayList<>();
+
+    public void addStrategy(DrivingStrategy strategy) {
+        strategies.add(strategy);
+    }
+    @Override
+    public boolean isApplicable(HashMap<Coordinate, MapTile> explored, CarController controller) {
+        for(DrivingStrategy strategy : strategies) {
+            if (strategy.isApplicable(explored, controller)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Coordinate getNextMove(HashMap<Coordinate, MapTile> explored, CarController controller) throws Exception {
+        for (DrivingStrategy strategy : strategies) {
+            if (strategy.isApplicable(explored, controller)) {
+                Coordinate nextMove = strategy.getNextMove(explored, controller);
+                if (nextMove == null) {
+                    throw new Exception("Error: Applicable strategy returned null!");
+                }
+
+                return nextMove;
+            }
+        }
+        return null;
+    }
+}
